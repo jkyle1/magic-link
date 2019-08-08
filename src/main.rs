b3420a9067fe0cbb8e08e6c109bf07ae6d4253c8
kyle.smallwood@gmail.com <kyle.smallwood@gmail.com>
2019-08-08 21:27:13 +0100

    added imports to clear errors

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{server, HttpResponse, Responder};
+use actix_web::{server, HttpResponse, Responder, HttpRequest, App};
 use actix_web::http::{StatusCode, Method, header};
 
 fn main() {
