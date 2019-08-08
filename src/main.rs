4a82ab5accc365416c0d8d754eb8ebe434903e3e
kyle.smallwood@gmail.com <kyle.smallwood@gmail.com>
2019-08-08 22:04:35 +0100

    added session storage middleware for encrypted email cookie; added login routes and route handler for login page

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,37 @@
-use actix_web::{server, HttpResponse, Responder, HttpRequest, App};
+use actix_web::{server, HttpResponse, Responder, HttpRequest, App, Result};
 use actix_web::http::{StatusCode, Method, header};
+use base64;
+use std::env;
+use actix_web::middleware::session::{CookieSessionBackend, SessionStorage};
 
-fn main() {
-    server::new(|| {
-        App::new().route("/", Method::GET, home)
-    })
-        .bind("0.0.0.0:80").unwrap()
-        .run()
-}
+
 
 fn home(request: HttpRequest) -> impl Responder {
     HttpResponse::build(StatusCode::OK)
         .content_type("text/html; charset=utf-8")
         .body("Welcome Home!".to_string())
 }
+
+fn login_page(_: HttpRequest) -> Result<HttpResponse> {
+    Ok(HttpResponse::build(StatusCode::OK)
+        .content_type("text/html; charset=utf-8")
+        .body(include_str!("../static/login.html")))
+}
+
+fn main() {
+
+    //key to encrypt and decrypt private cookie
+    let cookie_secret = base64::decode(&env::var("COOKIE_SECRET_KEY").unwrap()).unwrap();
+
+    server::new(|| {
+        App::new().middleware(SessionStorage::new(CookieSessionBackend::private(&cookie_secret).secure(false))) //secure = false in order to test without TLS on localhost
+            .route("/", Method::GET, home)
+    })
+        .bind("0.0.0.0:80").unwrap()
+        .run()
+
+        .route("/login", Method::GET, login_page)
+        .route("/login", Method::POST, login_submit)
+
+}
+
